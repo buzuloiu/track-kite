543b36b5d99f8bc633194ce0a9ae1751b6569359
NikhilPeri <nikhil.peri96@gmail.com>
2018-11-20 16:14:18 -0500

    Human Supervised Control

--- human_supervised_control.py
+++ human_supervised_control.py
@@ -1,23 +1,22 @@
 from src.controllers import XboxController, RoboController
 from src.actuators import connect_actuator
+import time
+
+CONTROL_LATENCY=0.5
 
 if __name__ == '__main__':
-    # actuator = connect_actuator()
+    active = True
+
     human_controller = XboxController()
     robot_controller = RoboController()
+    actuator = connect_actuator()
 
-    active_controller = human_controller
+    while active:
+        delta = None
+        if human_controller.active():
+            delta = human_controller.compute_delta()
+        else:
+            delta = robot_controller.compute_delta()
 
-    def human_take_control(button):
-        import pdb; pdb.set_trace()
-        active_controller = human_controller
-
-    def human_release_control(button):
-        import pdb; pdb.set_trace()
-        active_controller = robot_controller
-
-    human_controller.controller.button_a.when_pressed = human_release_control
-    human_controller.controller.button_a.when_released = human_take_control
-
-    while True:
-        print active_controller.compute_delta()
+        time.sleep(max(0, time.time()-actuator.last_update))
+        actuator.set_delta(delta)
--- src/controllers.py
+++ src/controllers.py
@@ -1,28 +1,27 @@
-from time import time
 from xbox360controller import Xbox360Controller
-MAX_STEPS_SECOND = 1./200.
+from random import randint
 
 class RoboController(object):
     def __init__(self):
         pass
 
     def compute_delta(self):
-        return "ROBOT"
+        return randint(-50, 50)
 
 class XboxController(object):
-    def __init__(self):
+    def __init__(self, gain=2):
+        self.gain = gain
         self.controller = Xbox360Controller(0, axis_threshold=0.1)
         self.last_read = 0
         self.current_delta = 0.0
 
     def compute_delta(self):
-        if (time() - self.last_read) < MAX_STEPS_SECOND:
-            return self.current_delta
-
         left_trigger = self.controller.trigger_l.value
         right_trigger = self.controller.trigger_r.value
 
-        self.current_delta += (left_trigger - right_trigger)
-        self.last_read = time()
+        self.current_delta += self.gain*(left_trigger - right_trigger)
 
         return self.current_delta
+
+    def active(self):
+        return not self.controller.button_a.is_pressed
