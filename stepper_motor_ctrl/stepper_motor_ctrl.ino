a5602683763ec71cade016c6b0fdefb3b21f9fef
azqlin <anthonyzqlin@gmail.com>
2018-11-20 18:52:42 -0500

    Char array method v0.1

--- stepper_motor_ctrl/stepper_motor_ctrl.ino
+++ stepper_motor_ctrl/stepper_motor_ctrl.ino
@@ -13,7 +13,7 @@ void setup() {
   setup_motor(left_motor);
   setup_motor(right_motor);
 
-  Serial.begin(9600);
+  Serial.begin(2000000);
 }
 
 void move_motor(Motor motor, int num_steps) {
@@ -33,28 +33,41 @@ void move_motor(Motor motor, int num_steps) {
 }
 
 void loop() {
-  while (Serial.available() < 1) {
-    String read_str = Serial.readString();
-    String sign1 = read_str.substring(0,1);
-    String sign2 = read_str.substring(4,5);
-    String str1 = read_str.substring(1, 4);
-    String str2 = read_str.substring(5, 8);
-
-//    int sign1 = sign1s.toInt();
-//    int sign2 = sign2s.toInt();
-    int move1 = str1.toInt();
-    int move2 = str2.toInt();
-    if (sign1 != "+"){
-      move1 = -1*move1;
-    }
-    if (sign2 == "+"){
-      move2 = -1*move2;
+  char inRead[9] = {'\0'};
+  char tmp[5];
+  char textA[5];
+  char textB[5];
+  memset(tmp, 0, sizeof(tmp));
+      
+  while (Serial.available() >= 8) {
+    
+    for (byte i=0; i<8; i++){
+      inRead[i] = Serial.read();
     }
 
-    move_motor(left_motor, move1);
-    move_motor(right_motor, move2);
+    strncpy(tmp, inRead, 4);
+    snprintf(textA, sizeof(textA), "%-4s", tmp);
+    strncpy(tmp, &inRead[4], 4);
+    Serial.println(textA);
+    snprintf(textB, sizeof(textB), "%-4s", tmp);
+    Serial.println(textB);
+
+//    if (inRead[0] != "+") {
+//      move1 = -1*move1;
+//    }
+//    if (inRead[4] != "+") {
+//      move2 = -1*move2;
+//    }
+//    
+//    Serial.println(move1);
+//    Serial.println(move2);
+//    delay(1000);
+//    move_motor(left_motor, move1);
+//    move_motor(right_motor, move2);
+
+  inRead[0] = (char)0;
   }
-
+  
 //  while ( true ) {
 //    move_motor(left_motor, 100);
 //    move_motor(left_motor, -100);
@@ -62,4 +75,5 @@ void loop() {
 //    move_motor(right_motor, 100);
 //
 //  }
+
 }
