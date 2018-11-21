eb77bb2eb64dc88c0c4c733e944bfd01c61e245e
Paul Buzuloiu <pbuzu025@uottawa.ca>
2018-11-21 17:29:48 -0500

    needs better x-y

--- src/track.py
+++ src/track.py
@@ -22,7 +22,34 @@ class Camera(object):
         self.greenUpper = (27, 193, 205)
 
     def move_origin(self, point):
-        return (point[1]-540, point[0]-250)
+        return (point[1]-540, point[0]-25)
+
+    def undistort(self, frame, DIM=(1920, 1080), balance=1.0, dim2=None, dim3=None):
+        dim1 = frame.image.shape[:2][::-1]  # dim1 is the dimension of input image to un-distort
+        assert dim1[0]/dim1[1] == DIM[0]/DIM[1], \
+            "Image to undistort needs to have same aspect ratio as the ones used in calibration"
+        if not dim2:
+            dim2 = dim1
+        if not dim3:
+            dim3 = dim1
+        scaled_K = self.K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
+        scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
+        # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image.
+        new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, self.D, dim2, np.eye(3),
+                                                                       balance=balance)
+        map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, self.D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
+        frame.image = cv2.remap(frame.image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+        frame.image = imutils.rotate_bound(frame.image, 270)
+        return frame
+
+    def capture_frame(self):
+        return Frame(self.stream.read())
+
+    def capture_and_process(self):
+        frame = self.capture_frame()
+        frame = camera.undistort(frame)
+        frame.find_kite(self)
+        return frame
 
 
 class Frame(object):
@@ -34,79 +61,57 @@ class Frame(object):
     def set_center(self, center):
         self.center = center
 
+    def find_kite(self, camera):
+        blurred = cv2.GaussianBlur(self.image, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-def undistort(frame, camera, DIM=(1920, 1080), balance=1.0, dim2=None, dim3=None):
-    dim1 = frame.image.shape[:2][::-1]  # dim1 is the dimension of input image to un-distort
-    assert dim1[0]/dim1[1] == DIM[0]/DIM[1], \
-        "Image to undistort needs to have same aspect ratio as the ones used in calibration"
-    if not dim2:
-        dim2 = dim1
-    if not dim3:
-        dim3 = dim1
-    scaled_K = camera.K * dim1[0] / DIM[0]  # The values of K is to scale with image dimension.
-    scaled_K[2][2] = 1.0  # Except that K[2][2] is always 1.0
-    # This is how scaled_K, dim2 and balance are used to determine the final K used to un-distort image.
-    new_K = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(scaled_K, camera.D, dim2, np.eye(3), balance=balance)
-    map1, map2 = cv2.fisheye.initUndistortRectifyMap(scaled_K, camera.D, np.eye(3), new_K, dim3, cv2.CV_16SC2)
-    frame.image = cv2.remap(frame.image, map1, map2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
-    frame.image = imutils.rotate_bound(frame.image, 270)
-    return frame
+        # construct a mask for the color "green"
+        mask = cv2.inRange(hsv, camera.greenLower, camera.greenUpper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
 
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        center = None
 
-def get_x_y(frame, camera):
-    blurred = cv2.GaussianBlur(frame.image, (11, 11), 0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            if center:
+                self.set_center(camera.move_origin(center))
 
-    # construct a mask for the color "green"
-    mask = cv2.inRange(hsv, camera.greenLower, camera.greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+            if radius > 10:
+                cv2.circle(self.image, (int(x), int(y)), int(radius),
+                           (0, 255, 255), 2)
+        cv2.circle(self.image, center, 5, (0, 0, 255), -1)
+        camera.pts.appendleft(center)
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-    center = None
+        # draw a red line on the frame following the points
+        for i in range(1, len(camera.pts)):
+            if camera.pts[i - 1] is None or camera.pts[i] is None:
+                continue
+            thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
+            cv2.line(self.image, camera.pts[i - 1], camera.pts[i], (0, 0, 255), thickness)
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-        if center:
-            frame.set_center(camera.move_origin(center))
-
-        if radius > 10:
-            cv2.circle(frame.image, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
-    cv2.circle(frame.image, center, 5, (0, 0, 255), -1)
-    camera.pts.appendleft(center)
-
-    # draw a red line on the frame following the points
-    for i in range(1, len(camera.pts)):
-        if camera.pts[i - 1] is None or camera.pts[i] is None:
-            continue
-        thickness = int(np.sqrt(64 / float(i + 1)) * 2.5)
-        cv2.line(frame.image, camera.pts[i - 1], camera.pts[i], (0, 0, 255), thickness)
-    return frame
 
 # example
 if __name__ == "__main__":
     camera = Camera()
     # keep looping
     while True:
-        frame = Frame(camera.stream.read())
-        frame = get_x_y(frame, camera)
-        frame = undistort(frame, camera)
+        frame = camera.capture_and_process()
         cv2.imshow("Frame", frame.image)
-        key = cv2.waitKey(1) & 0xFF
-        print(frame.center)
-        print(frame.time)
+        print(frame.time, frame.center)
 
         # if the 'q' key is pressed, stop the loop
+        key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
             break
     camera.stream.stop()
