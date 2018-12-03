b06e2f744445ceb60a2f53c784d5ce68679a8af2
mginoya <ginoya.manit@gmail.com>
2018-12-03 16:42:30 -0500

    an update to robo

--- src/controllers.py
+++ src/controllers.py
@@ -1,12 +1,25 @@
 from xbox360controller import Xbox360Controller
 from random import randint
+import numpy as np
 
 class RoboController(object):
     def __init__(self):
-        pass
+        self.xRange = [-525, -475, -425, -325, -225, -125, -25, 0, 25, 125, 225, 325, 425, 475, 525]
+        self.xTable = [-1, -0.75, -0.60, -0.45, -0.40, -0.25, -0.05, 0, 0.05, 0.25, 0.40, 0.45, 0.60, 0.75, 1]
+        self.current_delta = 0.0
+        self.maximumDeflectMeters = 0.3
 
-    def compute_delta(self):
-        return randint(-5, 5)
+    def compute_delta(self, feedback):
+
+        currentX = feedback.x
+        currentY = feedback.y
+        currentT = feedback.t
+
+        commandedDeflection = np.interp(currentX, self.xRange, self.xTable)
+        self.maximumDeflectMeters = 0.3*commandedDeflection
+        self.current_delta = self.maximumDeflectMeters
+
+        return self.current_delta
 
 class XboxController(object):
     def __init__(self, gain=200):
